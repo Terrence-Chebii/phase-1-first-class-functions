405a60cb05d238190f566ce189e31c7b33b74a88
bigman <terrence.chebii@student.moringaschool.com>
2023-03-20 09:42:57 +0300

    done

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-
-function receivesAFunction(callback) {
-  callback();
+function receivesAFunction(callback){
+    callback();
 }
 
-
-function returnsANamedFunction() {
-  return function namedFunction() {
-    console.log("This is a named function.");
-  };
+function returnsANamedFunction(){
+    return function namedFunction(){
+        console.log("named function");
+    }
 }
 
-
-function returnsAnAnonymousFunction() {
-  return function() {
-    console.log("This is an anonymous function.");
-  };
+function returnsAnAnonymousFunction(){
+    return function (){
+        console.log("this is an anonymus function");
+    }
 }
